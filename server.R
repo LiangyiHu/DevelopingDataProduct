037d4605e09c86b80ea615bf528d4af704b9b97e
LiangyiHu <larry.hu.8701@gmail.com>
2015-04-18 19:22:41 -0700

    asfd
    asdf

--- server.R
+++ server.R
@@ -1,11 +1,11 @@
-makeapredict<-function(x,y,z){
-    predictinput<-data.frame(disp=x,wt=y,am=as.numeric(z))
-    predict(lm(mpg~disp+wt+am,data=mtcars),newdata=predictinput)[[1]]
+makeapredict<-function(x,y,z,c){
+    predictinput<-data.frame(cyl=x,disp=y,wt=z,am=as.numeric(z))
+    predict(lm(mpg~factor(cyl)+disp+wt+am,data=mtcars),newdata=predictinput)[[1]]
 }
 
 shinyServer(
     function(input,output){
-        output$estimate<-renderPrint({makeapredict(input$disp,input$weight,input$am)})
+        output$estimate<-renderPrint({makeapredict(input$cyl,input$disp,input$weight,input$am)})
     }
  
 )
--- ui.R
+++ ui.R
@@ -4,9 +4,11 @@ shinyUI(
     headerPanel("Vehicle mpg estimating"),
     
     sidebarPanel(
-        numericInput('disp','Please input the displacement value, value should be ranged (50,500).',200,min=50,max=500,step=1),
+        selectInput("cyl","NumberOfCylinder",choices=list("4","6","8")),
+        numericInput('disp','Please input the displacement value, unit is cu.in. value should be ranged between (50,500).',200,min=50,max=500,step=1),
         numericInput('weight','Please input the weight value, unit is 1000lbs, ranged between(1.5, 5.5)',2,min=1.5,max=5.5,step=0.01),
         checkboxInput("am","manual transmission",value=TRUE),
+
         submitButton('Submit')
         ),
     mainPanel(
